dfe43317a5758f0d4db1870aa6fb78725e4cde5f
Zsolt <oszika05@gmail.com>
2020-10-31 12:13:45 +0100

    Added database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { BinModule } from './bin/bin.module';
+import {Module} from '@nestjs/common';
+import {BinModule} from './bin/bin.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 
 @Module({
-  imports: [BinModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        BinModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule.forRoot()],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('HOST'),
+                port: +configService.get<number>('PORT'),
+                username: configService.get('USERNAME'),
+                password: configService.get('PASSWORD'),
+                database: configService.get('DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
